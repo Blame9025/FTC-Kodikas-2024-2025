aa582ffcba78ba79d9b759046638ca2e5b38aceb
Albert542 <andrei268@yahoo.com>
2024-12-09 23:04:33 +0200

    am facut clasa outtake pentru a ridica la o pozitie(test)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Outake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Outake.java
@@ -1,4 +1,63 @@
 package org.firstinspires.ftc.teamcode.Utils;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Outake {
+
+    private DcMotor motorOuttake1,motorOuttake2;
+    private boolean alreadyInActionOuttake = false;
+    private boolean upForIntake = false;
+    private boolean fullOuttakeExtended = false;
+
+    public enum Position {
+        DEFAULT(0),
+        UPFORINTAKE(50),
+        EXTENDED(250);
+
+        public final int val;
+
+        Position(int val) {
+            this.val = val;
+        }
+    }
+
+    public void OuttakeController(DcMotor definedIntakeMotor1, DcMotor definedIntakeMotor2) {
+        this.motorOuttake1 = definedIntakeMotor1;
+        this.motorOuttake2 = definedIntakeMotor2;
+
+        motorOuttake1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorOuttake2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motorOuttake1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorOuttake2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motorOuttake1.setTargetPosition(Intake.Position.DEFAULT.val);
+        motorOuttake2.setTargetPosition(Intake.Position.DEFAULT.val);
+    }
+
+    public void setPosition(Position target) {
+
+        if(alreadyInActionOuttake){
+            return;
+        }
+        alreadyInActionOuttake = true;
+
+        motorOuttake1.setTargetPosition(target.val);
+        motorOuttake2.setTargetPosition(target.val);
+
+        motorOuttake1.setPower(0.5);
+        motorOuttake2.setPower(0.5);
+
+        long startTime = System.currentTimeMillis();
+        while(motorOuttake1.isBusy() && motorOuttake2.isBusy()){
+            if(System.currentTimeMillis() - startTime > 5000){
+                break;
+            }
+        }
+
+
+        alreadyInActionOuttake = true;
+
+    }
+
 }
