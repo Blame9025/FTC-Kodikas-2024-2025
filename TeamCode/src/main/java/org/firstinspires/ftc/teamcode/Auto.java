acdfa30d713632e70f2c135d631eeb68d140063a
Albert542 <andrei268@yahoo.com>
2024-12-15 09:03:12 +0200

    schimbare ora 15:56 12.12.2024 la cex

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.OdometrySubsystem;
+import com.arcrobotics.ftclib.command.PurePursuitCommand;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.purepursuit.waypoints.GeneralWaypoint;
+import com.arcrobotics.ftclib.purepursuit.waypoints.StartWaypoint;
+import com.arcrobotics.ftclib.util.Timing;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Utils.Intake;
+import org.firstinspires.ftc.teamcode.Utils.IntakeLift;
+import org.firstinspires.ftc.teamcode.Utils.KodiOdometry;
+import org.firstinspires.ftc.teamcode.Utils.KodikasRobot;
+import org.firstinspires.ftc.teamcode.Utils.Outake;
+import org.firstinspires.ftc.teamcode.Utils.OuttakeLift;
+
+
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name = "Auto")
+public class Auto extends LinearOpMode {
+
+    DcMotor leftEncoder, rightEncoder;
+    Timing.Timer stop,delay;
+    private static final long DEBUG_TIMER = 2000;
+    private static final long delayTimer = 500;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        KodikasRobot robot = new KodikasRobot(
+                hardwareMap, telemetry
+        );
+        MecanumDrive drive = robot.getDriveSession();
+        Intake intake = robot.getIntakeSession();
+        IntakeLift intakeLift = robot.getIntakeLiftSession();
+        Outake outake = robot.getOutakeSession();
+        OuttakeLift outtakeLift = robot.getOutakeLiftsession();
+        DcMotor coreHexIntake = robot.getCoreHexIntake();
+
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+
+        imu.initialize(parameters);
+
+        KodiOdometry kodiOdometry = new KodiOdometry(imu, leftEncoder, rightEncoder);
+        OdometrySubsystem odometry = new OdometrySubsystem(kodiOdometry.getHolonomicOdometry());
+        waitForStart();
+
+        PurePursuitCommand PureppCmd = new PurePursuitCommand(
+                drive, odometry,
+                new StartWaypoint(odometry.getPose()),
+                new GeneralWaypoint(
+                        0 ,-90, Math.toRadians(0),
+                        0.6, 0.5, 30)
+
+        );
+        PureppCmd.schedule();
+
+        intake.extendIntake();
+
+        stop = new Timing.Timer(DEBUG_TIMER, TimeUnit.MILLISECONDS);
+        stop.start();
+        while(!stop.done()){
+            coreHexIntake.setPower(1);
+        }
+
+        intake.retractIntake();
+
+        delay.start();
+        while(!delay.done()) {
+            coreHexIntake.setPower(-0.5);
+        }
+
+
+        outtakeLift.closeGrabber();
+
+        PurePursuitCommand PureppCmd2 = new PurePursuitCommand(
+                drive, odometry,
+                new StartWaypoint(odometry.getPose()),
+                new GeneralWaypoint(
+                        -30 ,-90, Math.toRadians(45),
+                        0.6, 0.5, 30)
+
+        );
+        PureppCmd2.schedule();
+
+        outake.extendOuttake();
+
+        if(outake.getCurrentPositionOuttake() == Outake.Position.EXTENDED){
+            outtakeLift.upArmGrabber();
+        }
+
+        PurePursuitCommand PureppCmd3 = new PurePursuitCommand(
+                drive, odometry,
+                new StartWaypoint(odometry.getPose()),
+                new GeneralWaypoint(
+                        -35 ,-90, Math.toRadians(45),
+                        0.6, 0.5, 30)
+
+        );
+        PureppCmd3.schedule();
+
+        outtakeLift.openGrabber();
+
+
+
+
+
+
+
+
+    }
+}
