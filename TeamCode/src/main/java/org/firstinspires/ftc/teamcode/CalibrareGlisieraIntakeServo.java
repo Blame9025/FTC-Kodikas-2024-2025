ddbbaff2d4b698a981aafeea079a6e815d348596
Albert542 <andrei268@yahoo.com>
2025-01-10 23:29:04 +0200

    10.1.2025 ora 12:21 noaptea autonomie aicitinel (albert) facuta din masuratori pe teren 3 specimene + parcare (netestat)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrareGlisieraIntakeServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalibrareGlisieraIntakeServo.java
@@ -32,16 +32,19 @@ import java.util.concurrent.TimeUnit;
 
 @TeleOp
 public class CalibrareGlisieraIntakeServo extends LinearOpMode {
-    private Servo servoIntake1,servoIntake2;
+    private Servo servoGlisieraIntake1,servoGlisieraIntake2;
     private DcMotor coreHex;
     private Outake outtake;
     private Timing.Timer cooldown = new Timing.Timer(800, TimeUnit.MILLISECONDS);
 
 
     public void initHW(){
-        servoIntake1 = hardwareMap.servo.get("servoIntake1");
-        servoIntake2 = hardwareMap.servo.get("servoIntake2");
+        servoGlisieraIntake1 = hardwareMap.servo.get("servoGlisieraIntake1");
+        servoGlisieraIntake2 = hardwareMap.servo.get("servoGlisieraIntake2");
+        servoGlisieraIntake2.setDirection(Servo.Direction.REVERSE);
+
     }
+
 
 
     @Override
@@ -51,8 +54,34 @@ public class CalibrareGlisieraIntakeServo extends LinearOpMode {
 
             waitForStart();
 
-            servoIntake1.setPosition(0.4);
-            servoIntake2.setPosition(0.4);
+//            servoGlisieraIntake1.setPosition(0.4);
+//        servoGlisieraIntake2.setPosition(0.4);
+//        sleep(2000);
+//        servoGlisieraIntake1.setPosition(0.8);
+//        servoGlisieraIntake2.setPosition(0.8);
+        servoGlisieraIntake2.setPosition(0.9);
+        servoGlisieraIntake1.setPosition(0.9);
+        sleep(3000);
+        servoGlisieraIntake2.setPosition(0.5);
+        servoGlisieraIntake1.setPosition(0.5);
+
+
+            while(opModeIsActive()){
+
+
+//                sleep(10000);
+//
+//                servoGlisieraIntake1.setPosition(0.8);
+//                servoGlisieraIntake2.setPosition(0.8);
+//
+//                sleep(10000);
+//
+//                servoGlisieraIntake1.setPosition(0.4);
+//                servoGlisieraIntake2.setPosition(0.4);
+
+            }
+
+
 
     }
 }
